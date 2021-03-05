cf2e0ddd8ecf375e6ef7799aaf4002fad5b6bab9
logic-three-body <1552451580@qq.com>
2021-03-05 18:50:07 +0800

    audio script05

---
+++ Assets/Script/PlayerAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour {
+	public AudioClip bumpSound;//冰壶直接碰撞的声音
+
+	public AudioSource audioS=null;//音频源
+
+	private void OnTriggerEnter(Collider other)
+    {
+		if(other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(bumpSound);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
