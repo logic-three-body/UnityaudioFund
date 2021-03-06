463249376cf8e4ed2aa7755bb323c42e436b2b3b
logic-three-body <1552451580@qq.com>
2021-03-06 11:50:50 +0800

    Mouse audio

---
+++ Assets/Script/MouseAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour {
+
+    public AudioSource audioS;
+    public void PlayClip(AudioClip clip)
+    {
+        audioS.PlayOneShot(clip);
+    }
+}
