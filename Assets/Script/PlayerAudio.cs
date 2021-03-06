89d89b9f3f774d6d20ca9a07bdca04a45f0fedaa
logic-three-body <1552451580@qq.com>
2021-03-06 14:43:19 +0800

    Audio Transitioning

--- Assets/Script/PlayerAudio.cs
+++ Assets/Script/PlayerAudio.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class PlayerAudio : MonoBehaviour {
 	public AudioClip bumpSound;//冰壶直接碰撞的声音
 
 	public AudioSource audioS=null;//音频源
+
+    public AudioMixerSnapshot idle;
+    public AudioMixerSnapshot auxin;
+
+    public float fade_test=0.5f;
 
 	private void OnTriggerEnter(Collider other)
     {
@@ -13,12 +18,24 @@ public class PlayerAudio : MonoBehaviour {
         {
             audioS.PlayOneShot(bumpSound);
         }
+
+        if(other.CompareTag("EnemyZone"))
+        {
+            //print("hey,you comein");
+            auxin.TransitionTo(fade_test);//音效的淡入淡出过渡，参数为淡入淡出时间
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(bumpSound);
+        }
+
+        if (other.CompareTag("EnemyZone"))
+        {
+            idle.TransitionTo(fade_test);
+            //print("hey,you exit");
         }
     }
 }
